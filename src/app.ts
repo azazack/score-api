18fbdff4a33d380836af678fe70cad80f89b197f
azazack <zakaria.azf@pre-history.com>
2021-12-08 15:09:43 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,17 @@ import {createConnection} from "typeorm";
 import PlayerRoutes from "./routes/player";
 import ScoreRoutes from "./routes/score";
 import cors from "cors";
-import "dotenv/config";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const app = Express();
 app.use(Express.json());
 app.use(cors())
 
+const port = process.env.PORT || 5000;
+
 createConnection().then(async (connection) => {
-  app.listen('5000',() => console.log("server is up and running"))
+  app.listen(port,() => console.log("server is up and running"))
 
   //Player Routes
   app.use(PlayerRoutes,ScoreRoutes);
