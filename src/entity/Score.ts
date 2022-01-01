856de3a298aee73c4e8026b3616d4654f220139c
azazack <zakaria.azf@pre-history.com>
2022-01-01 23:28:29 +0100

    feat: fixes

--- src/entity/Score.ts
+++ src/entity/Score.ts
@@ -1,4 +1,4 @@
-import {BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToMany, OneToOne, ManyToOne,JoinColumn} from "typeorm";
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToMany, OneToOne, ManyToOne,OneToMany} from "typeorm";
 import {Player} from "./Player";
 import {Week} from "./Week";
 
@@ -16,9 +16,8 @@ export class Score extends BaseEntity {
     @ManyToMany((type) => Player, player => player.scores)
     players: Player[];
 
-    @OneToOne((type) => Player)
-    @JoinColumn()
-    winner:Player;
+    @ManyToMany((type) => Player, player => player.winner)
+    winner:Player[];
 
     @ManyToOne(() => Week, week => week.scores)
     week:Week
